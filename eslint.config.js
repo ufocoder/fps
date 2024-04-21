8e6f811f94a3bcadaa1bdfe4275afd67ae5e03f6
Sergey Ufocoder <xufocoder@gmail.com>
2024-04-21 20:28:31 +0300

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,19 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: { 
+      globals: globals.browser
+    }
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/ban-types": "off",
+      
+    },
+  }
 ];
