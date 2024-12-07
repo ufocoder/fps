f6493ff2cba4df89992187aa82beff4b767451df
d.pereverza <disel920@gmail.com>
2024-12-07 18:26:00 +0300

    Fix optimisation problem in lights

--- src/lib/ecs/systems/MinimapSystem.ts
+++ src/lib/ecs/systems/MinimapSystem.ts
@@ -6,16 +6,22 @@ import BoxComponent from "src/lib/ecs/components/BoxComponent";
 import CircleComponent from "src/lib/ecs/components/CircleComponent";
 import MinimapComponent from "src/lib/ecs/components/MinimapComponent";
 import PositionComponent from "src/lib/ecs/components/PositionComponent";
-// import LightComponent from "src/lib/ecs/components/LightComponent.ts";
-// import LightSystem from "src/lib/ecs/systems/LightSystem.ts";
 import RenderSystem from "src/lib/ecs/systems/RenderSystem";
 
 export default class MinimapSystem extends System {
-  public readonly componentsRequired = new Set([MinimapComponent, PositionComponent]);
+  public readonly componentsRequired = new Set([
+    MinimapComponent,
+    PositionComponent,
+  ]);
 
   protected readonly scale: number = 20;
   protected readonly canvas: Canvas;
-  protected readonly offset: { top?: number, left?: number, bottom?: number, right?: number } = {top: 0, left: 0, bottom: 10, right: 20}
+  protected readonly offset: {
+    top?: number;
+    left?: number;
+    bottom?: number;
+    right?: number;
+  } = { top: 0, left: 0, bottom: 10, right: 20 };
   protected readonly container: HTMLElement;
 
   constructor(ecs: ECS, container: HTMLElement, level: Level) {
@@ -26,14 +32,13 @@ export default class MinimapSystem extends System {
 
     this.container = container;
 
-
-    let style = 'z-index: 3;position: absolute;';
+    let style = "z-index: 3;position: absolute;";
     if (this.offset.right) style += `right: ${this.offset.right}px;`;
     if (this.offset.top) style += `top: ${this.offset.top}px;`;
     if (this.offset.left) style += `left: ${this.offset.left}px;`;
     if (this.offset.bottom) style += `bottom: ${this.offset.bottom}px;`;
     this.canvas = new Canvas({
-      id: 'minimap',
+      id: "minimap",
       height: rows * this.scale,
       width: cols * this.scale,
       style,
@@ -46,7 +51,7 @@ export default class MinimapSystem extends System {
 
   update(_: number, entities: Set<Entity>) {
     this.canvas.clear();
-    this.canvas.drawBackground('green');
+    this.canvas.drawBackground("green");
 
     const renderSystem = this.ecs.getSystem(RenderSystem)!;
 
@@ -54,7 +59,9 @@ export default class MinimapSystem extends System {
       const components = this.ecs.getComponents(entity);
       const { color } = components.get(MinimapComponent);
 
-      const renderer = renderSystem.mapEntityRenders.find(render => render.canRender(components!));
+      const renderer = renderSystem.mapEntityRenders.find((render) =>
+        render.canRender(components!),
+      );
       if (renderer) {
         const edges = renderer.getArmature(components);
         this.drawPolygon(edges, color);
@@ -80,7 +87,7 @@ export default class MinimapSystem extends System {
 
   drawPolygon(paths: number[], color: string) {
     this.canvas.drawPolygon({
-      paths: paths.map(point => point * this.scale),
+      paths: paths.map((point) => point * this.scale),
       color,
     });
   }
@@ -103,5 +110,4 @@ export default class MinimapSystem extends System {
       color,
     });
   }
-
 }
